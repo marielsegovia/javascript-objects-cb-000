9ea9d9370b3a8cefdcab789ef33f7d0d8b4e8a6c
Mariel Segovia <msegovia94@gmail.com>
2018-04-24 06:04:30 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,15 +1,19 @@
 //Assign an object to the variable playlist and initialize the object with a key-value pair —
 //the keys will be artist names and the values will be song titles.
-//(What limitation does this impose on our playlist?)
 
 var playlist = new Object({artistName: 'songTitle'})
 
-
 //function update playlist, three parameters, playlist-object, artist name- string, songtitle
 //body of the function should add the song and artist as a key-value pair to the playlist object
-//The function should return the whole playlist.
+//The function should return the whole playlist
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
+}
+
+
+function removeFromPlaylist(playlist, artistName){
+  //delete key-value pair from playlist and return updated playlist
+  delete playlist.artistName
 }
